2b9a856b314ccb21f8f7a7d4ef907b54d40a5d20
Stephane NANGUE <stephaneleonel@gmail.com>
2023-04-10 17:45:35 +0200

    ajout du script de configuration automatique de Git

---
+++ auto-config-git.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Git global configuration
+git config --list --global
+git config --global user.name "Stephane NANGUE"
+git config --global user.email "stephaneleonel@gmail.com"
+git config --global init.defaultBranch main
+git config --list --global
