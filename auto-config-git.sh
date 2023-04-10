30d7e1560c7fd2a8a80f574ad6c0263c9e0feb47
Stephane NANGUE <stephaneleonel@gmail.com>
2023-04-10 18:14:12 +0200

    ajout des scripts de configuration automatique du projet Git

--- auto-config-git.sh
+++ auto-config-git.sh
@@ -6,3 +6,8 @@ git config --global user.name "Stephane NANGUE"
 git config --global user.email "stephaneleonel@gmail.com"
 git config --global init.defaultBranch main
 git config --list --global
+
+# Git project configuration
+git remote -v
+git remote add my_repo https://stephnangue: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@github.com/stephnangue/cloned_repo.git
+git remote show my_repo
